9ad0ad151680c3625e731b38820e02f87ff67200
Bernardo Souza de Oliveira <sdo.bernardo@gmail.com>
2025-02-09 15:20:11 -0300

    Inserção do spring como base e reestruturação do projeto

--- entrypoint/build.gradle.kts
+++ entrypoint/build.gradle.kts
@@ -1,7 +1,18 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 dependencies {
     implementation(project(":application"))
-}
+    implementation(project(":infrastructure"))
+
+    implementation("org.springframework.boot:spring-boot-starter:3.4.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")}
